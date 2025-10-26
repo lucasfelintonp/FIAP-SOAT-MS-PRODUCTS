a4fa1f1bcc97a0f4d1f96d8dbf74357546e0b9d4
Lucas Felinto <lucasfelintonp@gmail.com>
2025-10-25 21:44:44 -0300

    initial commit

---
+++ src/test/java/br/com/fiap/fastfood/products/FastfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fastfood.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
